c98d40017907a6ea9d2db21951517d6a030983ca
Andreef76 <andreef76@gmail.com>
2023-05-12 14:43:06 +0500

    commit lesson2.2

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,51 +3,59 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.utils.NumberConverter.convertCount
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val post = Post (
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            published = "21 мая в 18:36",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            likedByMe = false,
-            likes = 54,
-            shareCount = 997,
-            viewsCount = 5001
 
-        )
+// ==========================================================================================
 
-        with(binding) {
-            author.text = post.author
-            published.text = post.published
-            content.text = post.content
-            likeCount.text = post.likes.toString()
-            shareCount.text = post.shareCount.toString()
-            viewsCount.text = post.viewsCount.toString()
+//        binding.root.setOnClickListener  {
+//            Toast.makeText(this,"Root is clicked",Toast.LENGTH_SHORT).show()
+//        }
+//
+//        binding.avatar.setOnClickListener {
+//            Toast.makeText(this,"Avatar is clicked",Toast.LENGTH_SHORT).show()
+//        }
+// ==========================================================================================
 
-            if(post.likedByMe) {
-                like.setImageResource(R.drawable.ic_baseline_favorite_red_24)
-            }
+        val viewModel by viewModels<PostViewModel>()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                published.text = post.published
+                content.text = post.content
+                likeCount.text = convertCount(post.likes)
+                shareCount.text = convertCount(post.shareCount)
+                viewsCount.text = convertCount(post.viewsCount)
 
-            like.setOnClickListener {
-                post.likedByMe = !post.likedByMe
-                if (post.likedByMe) post.likes++ else post.likes--
                 like.setImageResource(if (post.likedByMe) R.drawable.ic_baseline_favorite_red_24 else R.drawable.ic_baseline_favorite_border_24)
+
                 likeCount.text = convertCount(post.likes)
             }
-
-            share.setOnClickListener {
-                post.shareCount++
-                shareCount.text = convertCount(post.shareCount)
-            }
         }
+
+        binding.like.setOnClickListener {
+            viewModel.like()
+
+        }
+
+        binding.share.setOnClickListener {
+            viewModel.share()
+        }
+
+//        binding.removeRedEye.setOnClickListener {
+//            viewModel.removeRedEye()
+//        }
+
     }
 
 
